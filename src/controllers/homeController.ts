4da433bba1c60ec6005618c6099d9866467c6e94
diego <diego@pop-os.localdomain>
2022-03-23 21:46:02 -0300

    criacao de model e pegando dados do array

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,14 +1,54 @@
 import {Request, Response} from 'express';
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+
 export const home = (req: Request, res: Response)=>{
-    res.render('pages/home');
+    let list = Pet.getAll();
+
+    res.render('pages/home',{
+        menu: createMenuObject('all'),
+        banner:{
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    });
 }
 export const dogs = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('dog');
 
+    res.render('pages/home',{
+        menu: createMenuObject('dog'),
+        banner:{
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    });
 }
 export const cats = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('cat');
 
+    res.render('pages/home',{
+        menu: createMenuObject('cat'),
+        banner:{
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    });
 }
 export const fishes = (req: Request, res: Response)=>{
-
+    let list = Pet.getFromType('fish');
+    
+    res.render('pages/home',{
+        menu: createMenuObject('fish'),
+        banner:{
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        },
+        list
+    });
 }
